c082d4e147d4ead3cdde677ae6e3720d7632f5e0
Beatriz Rangel <raangelbeatriz@gmail.com>
2021-07-03 14:34:28 -0300

    add hightest and lowest solution

---
+++ HighestAndLowest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HighestAndLowest
+{
+    using System;
+
+public class Kata
+{
+  public string HighAndLow(string numbers)
+  {
+    String [] Numbers = numbers.Split(" ");
+    int length = Numbers.Length;
+
+    int [] NumbersInt = new int[length];
+    int nHigher = Int32.Parse(Numbers[0]);
+    int nLower = Int32.Parse(Numbers[0]);
+    
+    for(int i = 0; i <= length-1; i++){
+      NumbersInt[i] = Int32.Parse(Numbers[i]);
+    }
+     
+   foreach (int n in NumbersInt){
+      if (nHigher < n){
+        nHigher = n;
+      }
+      if (nLower > n){
+        nLower = n;
+      }
+   }
+   String answer = nHigher + " " + nLower;
+   Console.WriteLine(answer);
+   return answer;
+  }
+
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Kata k = new Kata();
+        k.HighAndLow("2 3 4 6 -2 70 90 -6");
+    }
+}
+}
---
+++ HighestAndLowest/README.md
@@ -0,0 +1,8 @@
+# Hightest and Lowest
+
+My solution to the algorithm problem proposed by CodeWars, in which I choosed to use the c# language.
+
+## The problem
+You are given a string of space separated numbers, and have to return the highest and lowest number.
+
+- The output need to be two numbers separated by a single space, and highest number is first.
