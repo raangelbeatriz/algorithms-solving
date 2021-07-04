3ca730c8b1d255b59f82949612965aa8121f0c44
Beatriz Rangel <raangelbeatriz@gmail.com>
2021-07-04 12:54:41 -0300

    add growth of population

---
+++ GrowthofPopulation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace GrowthofPopulation
+{
+
+
+class Arge {
+    
+    
+    public static int NbYear(int p0, double percent, int aug, int p) {
+      int countYear = 0;
+      
+      while (p0 < p){
+        p0 += (int)(aug + ((p0 * percent)/100));
+        countYear++;
+      }
+      Console.WriteLine(countYear);
+      return countYear;
+
+    }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arge.NbYear(1500, 5, 100, 5000);
+        }
+    }
+}
